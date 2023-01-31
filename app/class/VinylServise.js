cbcc4aa72a625046db3f78ecf48251220a312e14
ceotyz <ceotyz@mail.ru>
2023-01-31 18:27:39 +0300

    add storage

--- app/class/VinylServise.js
+++ app/class/VinylServise.js
@@ -1,15 +1,18 @@
 const Vinyl = require("./Vinyl");
+const NedbPromises = require('nedb-promises');
 
 class VinylServise {
 	constructor() {
-		
+		this.storage = NedbPromises.create({filename: './app/db/nedb/vinyls'});
 	}
-	
 	add(artist, title) {
 		let v1 = new Vinyl(artist, title);
-		console.log(v1);
+		this.storage.insert(v1); 
+		//console.log(this.storage);
 	}
-
+	async get() {
+		return await this.storage.find({});
+	}
 }
 module.exports = VinylServise;
 
--- index.js
+++ index.js
@@ -1,68 +1,19 @@
-
 const express = require('express');
 const app = express();
+const bodyParser = require('body-parser');
 
 let VinylServise = require("./app/class/VinylServise");
+let vinylServise = new VinylServise();
 
-app.get('/', function(req, res) {
-  res.send('get all');
+app.use(bodyParser.json());
+app.get('/', async function(req, res) {
+  res.send(await vinylServise.get());
 })
 
 app.post('/vinyl/', function(req, res) {
-	
-  
-  let vinylServise = new VinylServise();
-  vinylServise.add("Abba", "Best80");
-  
+  vinylServise.add(req.body.artist, req.body.title);
   res.send('add');
 });
 
 app.listen(3000);
 console.log("Im working")
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-/*
-const Vinyl = require("./app/class/Vinyl");
-let v1 = new Vinyl("Abba", "Best80");
-let v2 = new Vinyl("beatles", "Yellowsudmarine");
-
-console.log(v1);
-console.log(v2);
-
-v1.rename("Best90");
-console.log(v1);
-*/
-/*
-let a = 1;
-let b = a;
-
-a = 2;
-console.log(b);
-*/
-
-//console.log("tut");
-
